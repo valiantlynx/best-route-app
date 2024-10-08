a24aee10da61b1bb8dc41bb27ae44a2a88c2314a
Max <maxh@uia.no>
2024-10-08 13:59:11 +0200

    Task 1, correct 0.01 istedenfor 0.1

--- Task1.py
+++ Task1.py
@@ -5,7 +5,7 @@ import plotly.express as px
 # Plot the function f(x) = -0.69x^2 + 1.3x + 0.42 over the interval [0, 2.5], with 0.01 increments in x.
 def f(x):
     return -0.69 * x ** 2 + 1.3 * x + 0.42
-x_values = np.arange(0, 2.5, 0.1)
+x_values = np.arange(0, 2.5, 0.01)
 y_values = f(x_values)
 fig = px.line(x=x_values, y=y_values, title="F(x)= -0.69x^2 + 1.3x + 0.42")
 fig.show()
@@ -20,5 +20,5 @@ for i in range(2,n+1):
 xs = list(range(1, n + 2))
 print(xs)
 
-fig = px.scatter(x=xs, y=fib, title="Fibbonachi Scatter Plot")
+fig = px.scatter(x=xs, y=fib, title="Fibonacci Scatter Plot")
 fig.show()
